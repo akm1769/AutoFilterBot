ae29d9dd8566fab748063dd2d4bd0454eff96957
AMOGHA KRISHNA M <amoghakrishnam99@gmail.com>
2026-07-19 20:28:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -75,7 +75,7 @@ STAR_PREMIUM_PLANS = {
 # ============================
 # MongoDB Configuration
 # ============================
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://amoghakrishnam99:Uj9xyQygBABW0c27@cluster0.rwttrl2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")  # MongoDB URI for the database
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://amoghakrishnam99:Uj9xyQygBABW0c27@cluster0.rwttrl2.mongodb.net/?appName=Cluster0")  # MongoDB URI for the database
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0") # Database name (default: cluster)
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'dreamcinezone_files') # Collection name (default: dreamcinezone_files)
 
