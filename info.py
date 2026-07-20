dec7808151978896417fee3fb40b1f00cfd7d1bc
AMOGHA KRISHNA M <amoghakrishnam99@gmail.com>
2026-07-20 15:57:28 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,9 +18,9 @@ def is_enabled(value, default):
 # Bot Information Configuration
 # ============================
 SESSION = environ.get('SESSION', 'novabotz_search')   # Session name for the bot
-API_ID = int(environ.get('API_ID', '10408464')) # API ID from my.telegram.org
-API_HASH = environ.get('API_HASH', ' e683a898b9e30ccce1fc69c5e186448e')  # API Hash from my.telegram.org
-BOT_TOKEN = environ.get('BOT_TOKEN', "7888042647:AAHbpo7pRcg1NQq48-uTeJPfMWoTGKk0pP8")    # Bot token from @BotFather
+API_ID = int(environ.get('API_ID')) # API ID from my.telegram.org
+API_HASH = environ.get('API_HASH')  # API Hash from my.telegram.org
+BOT_TOKEN = environ.get('BOT_TOKEN')    # Bot token from @BotFather
 
 # ============================
 # Bot Settings Configuration
@@ -75,7 +75,7 @@ STAR_PREMIUM_PLANS = {
 # ============================
 # MongoDB Configuration
 # ============================
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://amoghakrishnam99:Uj9xyQygBABW0c27@cluster0.rwttrl2.mongodb.net/?appName=Cluster0")  # MongoDB URI for the database
+DATABASE_URI = environ.get('DATABASE_URI')  # MongoDB URI for the database
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0") # Database name (default: cluster)
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'dreamcinezone_files') # Collection name (default: dreamcinezone_files)
 
